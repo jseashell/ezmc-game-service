6bc6977c28e012de16d466f0fb3257b515c035e2
John Schellinger <john.schellinger@genesys.com>
2023-02-10 17:06:04 -0500

    Simplify promise chains

--- src/functions/ip-address/handler.ts
+++ src/functions/ip-address/handler.ts
@@ -1,5 +1,12 @@
-import { DescribeInstancesCommand, EC2Client } from '@aws-sdk/client-ec2';
-import { DescribeContainerInstancesCommand, ECSClient, ListContainerInstancesCommand } from '@aws-sdk/client-ecs';
+import { DescribeInstancesCommand, DescribeInstancesResult, EC2Client } from '@aws-sdk/client-ec2';
+import {
+  ContainerInstance,
+  DescribeContainerInstancesCommand,
+  DescribeContainerInstancesResponse,
+  ECSClient,
+  ListContainerInstancesCommand,
+  ListContainerInstancesResponse,
+} from '@aws-sdk/client-ecs';
 import { formatJsonError, formatJsonResponse } from '@libs/api-gateway';
 import { buildClusterArn } from '@libs/ecs';
 import { middyfy } from '@libs/lambda';
@@ -8,58 +15,55 @@ import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from 'aws-l
 const ipAddress: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> = async (event) => {
   const clusterName = event.queryStringParameters.clusterName;
   const ecsClient = new ECSClient({ region: process.env.REGION });
-
   return ecsClient
     .send(
       new ListContainerInstancesCommand({
         cluster: buildClusterArn(clusterName),
       }),
     )
-    .then((res) => {
+    .then((res: ListContainerInstancesResponse) => {
       if (res.containerInstanceArns?.length > 0) {
-        return ecsClient
-          .send(
-            new DescribeContainerInstancesCommand({
-              cluster: buildClusterArn(clusterName),
-              containerInstances: [res.containerInstanceArns[0]],
-            }),
-          )
-          .then((res) => {
-            if (res.containerInstances?.length > 0) {
-              const instanceId = res.containerInstances[0].ec2InstanceId;
-              const ec2Client = new EC2Client({ region: process.env.REGION });
-              return ec2Client
-                .send(
-                  new DescribeInstancesCommand({
-                    Filters: [
-                      {
-                        Name: 'instance-id',
-                        Values: [instanceId],
-                      },
-                    ],
-                  }),
-                )
-                .then((res) => {
-                  const ipAddress = res.Reservations?.[0].Instances?.[0].PublicIpAddress;
-                  return formatJsonResponse({
-                    ipAddress: ipAddress,
-                  });
-                })
-                .catch((err) => {
-                  console.error(err);
-                  return formatJsonError(err);
-                });
-            } else {
-              return formatJsonResponse({
-                ipAddress: '',
-              });
-            }
-          });
+        return res.containerInstanceArns[0];
       } else {
-        return formatJsonResponse({
-          ipAddress: '',
-        });
+        return Promise.reject(`No container instances in cluster "${clusterName}"`);
       }
+    })
+    .then((containerInstanceArn: string) => {
+      return ecsClient.send(
+        new DescribeContainerInstancesCommand({
+          cluster: buildClusterArn(clusterName),
+          containerInstances: [containerInstanceArn],
+        }),
+      );
+    })
+    .then((res: DescribeContainerInstancesResponse) => {
+      if (res.containerInstances?.length > 0) {
+        return res.containerInstances[0];
+      } else {
+        return Promise.reject('Unable to describe a container instance');
+      }
+    })
+    .then((containerInstance: ContainerInstance) => {
+      const instanceId = containerInstance.ec2InstanceId;
+      const ec2Client = new EC2Client({ region: process.env.REGION });
+      return ec2Client.send(
+        new DescribeInstancesCommand({
+          Filters: [
+            {
+              Name: 'instance-id',
+              Values: [instanceId],
+            },
+          ],
+        }),
+      );
+    })
+    .then((res: DescribeInstancesResult) => {
+      return res.Reservations?.[0].Instances?.[0].PublicIpAddress;
+    })
+    .then((ipAddress) => {
+      return formatJsonResponse({
+        ipAddress: ipAddress,
+      });
     })
     .catch((err) => {
       console.error(err);
--- src/functions/status/handler.ts
+++ src/functions/status/handler.ts
@@ -5,11 +5,10 @@ import { middyfy } from '@libs/lambda';
 import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from 'aws-lambda';
 
 const status: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> = async (event) => {
-  const client = new ECSClient({ region: process.env.REGION });
   const clusterName = event.queryStringParameters.clusterName;
-
   const serviceArn = await getServiceArn(clusterName);
 
+  const client = new ECSClient({ region: process.env.REGION });
   return client
     .send(
       new ListTasksCommand({
@@ -19,32 +18,27 @@ const status: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> = async (even
     )
     .then((res) => {
       if (res.taskArns?.length > 0) {
-        const taskArn = res.taskArns[0];
-        return client
-          .send(
-            new DescribeTasksCommand({
-              cluster: buildClusterArn(clusterName),
-              tasks: [taskArn],
-            }),
-          )
-          .then((res) => {
-            if (res.tasks?.length > 0 && res.tasks[0].containers?.length > 0) {
-              return formatJsonResponse({
-                status: res.tasks[0].containers[0].lastStatus,
-              });
-            } else {
-              return formatJsonResponse({
-                status: 'LAUNCHING',
-              });
-            }
-          })
-          .catch((err) => {
-            console.error(err);
-            return formatJsonError(err);
-          });
+        return res.taskArns[0];
+      } else {
+        return Promise.reject(`No tasks for service "${serviceArn}" in cluster "${clusterName}"`);
+      }
+    })
+    .then((taskArn) => {
+      return client.send(
+        new DescribeTasksCommand({
+          cluster: buildClusterArn(clusterName),
+          tasks: [taskArn],
+        }),
+      );
+    })
+    .then((res) => {
+      if (res.tasks?.length > 0 && res.tasks[0].containers?.length > 0) {
+        return formatJsonResponse({
+          status: res.tasks[0].containers[0].lastStatus,
+        });
       } else {
         return formatJsonResponse({
-          status: 'STOPPED',
+          status: 'LAUNCHING',
         });
       }
     })
